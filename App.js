fdc3663341cd3f02d3113f4c8f7cdd249e65bef8
QianXing Ou <qxou14@gmail.com>
2022-02-02 21:18:46 -0500

    test different branch

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Project</Text>
+      <Text>Hello Project11</Text>
       <StatusBar style="auto" />
     </View>
   );
