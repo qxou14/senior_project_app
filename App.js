90122bea755a56edd9ceb6372489fdf9bd514f88
QianXing Ou <qxou14@gmail.com>
2022-02-05 17:36:53 -0500

    added register page

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import SignIn from "./src/SignIn";
+import Register from "./src/Register";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <SignIn />
+      <Register />
     </View>
   );
 }
