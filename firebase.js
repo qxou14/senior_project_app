2ef09c56a1bf0f0f658614b3b00557e77e73be26
QianXing Ou <qxou14@gmail.com>
2022-02-12 23:04:44 -0500

    completed register system and connected to firebase

---
+++ firebase.js
@@ -0,0 +1,45 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+import "firebase/compat/auth";
+import "firebase/compat/database";
+
+import firebase from "firebase/compat/app";
+
+const {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+} = require("./keys.js");
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
+};
+
+// Initialize Firebase
+let app = firebase.initializeApp(firebaseConfig);
+
+if (firebase.app.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const auth = firebase.auth();
+
+export { auth };
--- src/SignIn.js
+++ src/SignIn.js
@@ -1,42 +1,41 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Pressable, Text, TextInput} from "react-native";
+import { StyleSheet, View, Pressable, Text, TextInput } from "react-native";
 import Input_button from "./Input_button";
 
 export default function SignIn({ navigation }) {
   return (
     <View style={styles.container}>
-
-        <View style={styles.inputView}>
-          <TextInput
-            style={styles.TextInput}
-            placeholder="Username"
-            placeholderTextColor="green"
-          />
-        </View>
-
-        <View style={styles.inputView}>
-          <TextInput
-            style={styles.TextInput}
-            placeholder="Password"
-            placeholderTextColor="green"
-          />  
-        </View>
-
-
-    <View style={styles.bottomContainer}>
-
-      <Pressable style = {styles.button} 
-                 onPress={() => console.log("hello world")}>
-        <Text style = {styles.buttonText}>Login</Text>
-      </Pressable>
-      <Pressable style = {styles.button}
-                 title="Register"
-                 onPress={() => navigation.navigate("Register")}>
-        <Text style = {styles.buttonText}>Register</Text>
-      </Pressable>
-
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Username"
+          placeholderTextColor="green"
+        />
       </View>
 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="green"
+        />
+      </View>
+
+      <View style={styles.bottomContainer}>
+        <Pressable
+          style={styles.button}
+          onPress={() => console.log("hello world")}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </Pressable>
+        <Pressable
+          style={styles.button}
+          title="Register"
+          onPress={() => navigation.navigate("Register")}
+        >
+          <Text style={styles.buttonText}>Register</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -49,20 +48,20 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 
-  bottomContainer:{
-    position : 'absolute',
-    top : 450 , 
+  bottomContainer: {
+    position: "absolute",
+    top: 450,
   },
 
-  inputView: {  
+  inputView: {
     backgroundColor: "#F6F6F6",
-    
+
     borderRadius: 30,
     width: "80%",
     height: 60,
     marginBottom: 20,
     alignItems: "center",
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 
   TextInput: {
@@ -73,20 +72,18 @@ const styles = StyleSheet.create({
   },
 
   button: {
-    
     alignItems: "center",
     backgroundColor: "#5DB075",
     paddingVertical: 20,
     paddingHorizontal: 100,
-    borderRadius: 100, 
-    marginBottom: 20, 
-    
+    borderRadius: 100,
+    marginBottom: 20,
   },
 
   buttonText: {
     fontSize: 20,
     fontWeight: "bold",
     letterSpacing: 0.25,
-    color: "white"
+    color: "white",
   },
 });
