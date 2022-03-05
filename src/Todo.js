deb8ee9bc7c8f47c919fe2a0c814bb68905e8dec
Yi Yang Wu <69625290+yywu123@users.noreply.github.com>
2022-03-05 05:15:39 -0500

    todo design for the caregiver
    todo design for the caregiver

--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,22 @@
 import { auth, db } from "../firebase";
-import { StyleSheet, View, Text, FlatList, Pressable, Dimensions,ScrollView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  Pressable,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import Checkbox from "expo-checkbox";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Foundation from 'react-native-vector-icons/Foundation';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-
-
+import Foundation from "react-native-vector-icons/Foundation";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 Foundation.loadFont();
 MaterialIcons.loadFont();
-const disensions = Dimensions.get('screen');
+const disensions = Dimensions.get("screen");
 
 export default function Todo({ navigation }) {
   const [Item, setItem] = useState([]);
@@ -45,76 +51,59 @@ export default function Todo({ navigation }) {
     });
   };
 
-
-  const ListItem = ({todo}) => {
-    return <View > 
-      <View style={styles.listItem}>
-        <View style = {styles.leftitem}>
-
-                <Pressable 
-                      onPress={() => {
-                        check_box(todo.key, todo.check, todo.Action, todo.Time);
-                      }}
-                    >
-                      <Checkbox style ={styles.checkbox} disabled value={todo.check} />
-                </Pressable>
-          <Text style = 
-          {{ fontWeight : 'bold', fontSize : 20,}}> 
-              {todo.key} </Text>
-
-
-        </View>
-
-        <View style = {styles.rightitem}>
-          <Text style = {{ fontWeight : 'bold',
-                                   fontSize : 20,  
-                    textDecorationLine : todo?.check ? 'line-through':'none', }}> 
-                     {todo.Action} </Text>
+  const ListItem = ({ todo }) => {
+    return (
+      <View>
+        <View style={styles.listItem}>
+          <View style={styles.leftitem}>
+            <Pressable
+              onPress={() => {
+                check_box(todo.key, todo.check, todo.Action, todo.Time);
+              }}
+            >
+              <Checkbox style={styles.checkbox} disabled value={todo.check} />
+            </Pressable>
+            <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+              {todo.key}{" "}
+            </Text>
           </View>
+
+          <View style={styles.rightitem}>
+            <Text
+              style={{
+                fontWeight: "bold",
+                fontSize: 20,
+                textDecorationLine: todo?.check ? "line-through" : "none",
+              }}
+            >
+              {todo.Action}{" "}
+            </Text>
+          </View>
+        </View>
       </View>
-    </View>
+    );
   };
 
   return (
-
-
-
-      <View style={styles.container}>
-        
-          
-
-          <View style={styles.topContainer}> 
-
-            <Foundation name= "calendar" size = {56} style={styles.calendar} />
-            <View style={styles.dataWrapper}>
-              <MaterialIcons name="keyboard-arrow-left" size = {46}/>
-              <Text style= {styles.dateStyle}>March 3 2022</Text>
-              <MaterialIcons name="keyboard-arrow-right" size = {46}/>
-            </View>
-
-          </View>
-
-
-
-                <FlatList
-
-                  data={Item}
-                  showsVerticalScrollIndicator= {false}
-                  contentContainerStyle = {{ paddingBottom : 100}}
-
-                  renderItem={({ item }) => <ListItem todo= {item} />}
-                  
-                  
-                />
-    
-       
-        
-
-        
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <Foundation name="calendar" size={56} style={styles.calendar} />
+        <View style={styles.dataWrapper}>
+          <MaterialIcons name="keyboard-arrow-left" size={46} />
+          <Text style={styles.dateStyle}>March 3 2022</Text>
+          <MaterialIcons name="keyboard-arrow-right" size={46} />
+        </View>
       </View>
+
+      <FlatList
+        data={Item}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        renderItem={({ item }) => <ListItem todo={item} />}
+      />
+    </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -124,61 +113,50 @@ const styles = StyleSheet.create({
     padding: 0,
   },
 
-  topContainer:{
-    
-    height : disensions.height / 4 ,
-    width : disensions.width,
-    alignContent: 'center',
-    justifyContent : 'center',
+  topContainer: {
+    height: disensions.height / 4,
+    width: disensions.width,
+    alignContent: "center",
+    justifyContent: "center",
   },
 
-  dataWrapper:{
-    flexDirection : 'row',
-    alignItems : 'center',
-    justifyContent : 'center',
-    
-  },  
+  dataWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-  dateStyle : {
-    fontWeight :"bold",
-    fontSize : 30 , 
-
+  dateStyle: {
+    fontWeight: "bold",
+    fontSize: 30,
   },
   calendar: {
-    textAlign : 'center',
+    textAlign: "center",
   },
 
-  
+  listItem: {
+    padding: 20,
+    backgroundColor: "white",
+    flexDirection: "row",
+    width: disensions.width * 0.9,
+    height: disensions.height / 10,
+    alignItems: "center",
+    justifyContent: "space-between",
 
-
-  listItem : {
-    padding:20,
-    backgroundColor : 'white',
-    flexDirection : 'row',
-    width : disensions.width * 0.9 ,
-    height : disensions.height / 10, 
-    alignItems : 'center',
-    justifyContent : 'space-between',
-
-    elevation : 12, 
-    borderRadius : 10 , 
-    marginVertical : 20 , 
+    elevation: 12,
+    borderRadius: 10,
+    marginVertical: 20,
   },
 
-
-  leftitem : {
-    flexDirection : "row" ,
-    alignItems : 'center' , 
+  leftitem: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 
-  checkbox :{
-    width : 25,
-    height : 25 , 
-    borderRadius : 5,
-    marginRight : 15 ,
+  checkbox: {
+    width: 25,
+    height: 25,
+    borderRadius: 5,
+    marginRight: 15,
   },
-
-
-
- 
 });
